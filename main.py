ff271c78ba7bebd31b26ad479f2c857e6eaf241a
ShermanWoo <sherman@ShermanWoos-MacBook-Air.local>
2024-12-05 15:52:07 +0000

    Created main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+import platform
+
+python_version = platform.python_version()
+
+if python_version == "3.10.6" :
+	print(f"I am using Python Version {python_version}")
+
+else:
+	print(f"I am using another Python Version {python_version}")
+
+
